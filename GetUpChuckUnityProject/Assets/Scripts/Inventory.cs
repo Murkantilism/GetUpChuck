61caad1104d1001181b7fc90ea0f9c35ea44683d
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2015-01-18 21:09:10 -0500

    Working on abstract classes, see description pls
    -Minor reorganization of folders
    -Made Inventory.cs and Item.cs abstract classes
    -Modified and added a few fields to both abstract classes
    -Added several functions to both, some still need to be implemented

--- GetUpChuckUnityProject/Assets/Scripts/Inventory.cs
+++ GetUpChuckUnityProject/Assets/Scripts/Inventory.cs
@@ -1,37 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Inventory : MonoBehaviour {
+public abstract class Inventory : MonoBehaviour {
 
 	//inventory size (max weight)
-	public int inventorySize;
+	public float inventorySize;
 
 	//keep traks of how much weight the player is currently carrying
-	public int currentWeight;
+	public float currentWeight;
+
+	// Declare ArrayList to store list of items
+	ArrayList loItems = new ArrayList();
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-	//TODO structure to store items in
-
-	//TODO add items to structure, increment weight
-
-	//TODO remove items from structure, decrease weight
-
-	//geter ans seterfor currentWeight
-	public int getCurrentWeight(){
+	// Geter and seter for currentWeight
+	public float getCurrentWeight(){
 		return this.currentWeight;
 	}
 
-	public void setCurrentWeight(int tmpWeight){
+	public void setCurrentWeight(float tmpWeight){
 		this.currentWeight = tmpWeight;
+	}
+
+	// Add items to structure, increment weight
+	public void AddItem(Item item){
+		loItems.Add(item);
+		currentWeight += item.getWeight();
+	}
+	
+	//Remove item from structure, decrease weight
+	public void DropItem(Item item){
+		loItems.Remove(item);
+		currentWeight -= item.getWeight();
+		// TODO: Call a function to instantiate item in world space
+	}
+
+	// Delete the item from inventory, DO NOT drop it back into world space
+	public void DeleteItem(Item item){
+		loItems.Remove(item);
+		currentWeight -= item.getWeight();
+	}
+
+	// Called by each individual item once per second of game time.
+	public void DigestionTick(Item item){
+		// If the timer hasn't expired, subtract 1 from the digst timer
+		if(item.getDigestionTimer() > 0){
+			item.setDigestionTimer(item.getDigestionTimer() - 1.0f);
+		// If the timer has expired, delete the item fromt the inventory
+		}else if(item.getDigestionTimer() <= 0){
+			DeleteItem(item);
 		}
+	}
+
+	// Given an Array of items check if any can be combined with each other.
+	void CheckItemCombinations(Item[] items){
+
+	}
 
 }
--- GetUpChuckUnityProject/Assets/Scripts/Item.cs
+++ GetUpChuckUnityProject/Assets/Scripts/Item.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item : MonoBehaviour {
+public abstract class Item : MonoBehaviour {
 
 	//stores weight of item
-	public int weight;
+	public float weight;
 
 	//stores hue
-	//red = 1
-	//blue = 2
+	//red = 0
+	//blue = 1
 	public int hue;
 
 	//name of object
-	public string name;
+	public string item_name;
 
+	// How long it takes for the item to degrade (if set to 1337 item cannot be digested)
+	public float digestionTimer;
+
+	// An array of potential item combinations allowed with this item
+	public string[] potentialCombinations;
+
+	// A timer to keep track of when a second of game time has occurred
+	float oneSecTimer;
 
 	// Use this for initialization
 	void Start () {
@@ -22,39 +30,74 @@ public class Item : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// If the item can be digested 
+		if(digestionTimer != 1337){
+			// Increment timer
+		    oneSecTimer += Time.deltaTime;
+			// Once a second of game time is reached, call DigestionTick()
+			if(oneSecTimer > 1){
+				// TODO: Get a reference to the Inventory classes for Red & Blue
+				// then call the DigestionTick() function, passing ourself as the arg
+			}
+			oneSecTimer = 0; // Reset timer
+		}
 	}
 
 	//geter and seter for weight
-	public int getWeight (){
+	public float getWeight (){
 		return this.weight;
-		}
-
-	public void setWeight(int inWeight){
+	}
+	public void setWeight(float inWeight){
 		this.weight = inWeight;
-		}
+	}
 
 	//geter and seter for hue
 	public int getHue(){
 		return this.hue;
-		}
-
+	}
 	public void setHue (string tmpHue){
 		if (tmpHue.Equals ("red")) {
-			this.hue = 1;
-				}
-		if (tmpHue.Equals ("blue")) {
-			this.hue = 2;
-				}
+			this.hue = 0;
 		}
+		if (tmpHue.Equals ("blue")) {
+			this.hue = 1;
+		}
+	}
 
 	//geter and seter for name
 	public string getName(){
-		return this.name;
-		}
-
+		return this.item_name;
+	}
 	public void setName(string tmpName){
-		this.name = tmpName;
+		this.item_name = tmpName;
+	}
+
+	// Geter and seter for digestion timer
+	public float getDigestionTimer(){
+		return this.digestionTimer;
+	}
+	public void setDigestionTimer(float tmpTimer){
+		this.digestionTimer = tmpTimer;
+	}
+
+	// Geter and seter for list of combinations
+	public string[] getCombinations(){
+		return this.potentialCombinations;
+	}
+	public void setCombinations(string[] tmpCombos){
+		this.potentialCombinations = tmpCombos;
+	}
+
+	// Add this item to the inventory system
+	void AddToInventory(){
+		// TODO: Call the AddItem() function of the target inventory script
+		// TODO: Call the HonorableSeppuku() function once item is added
+	}
+
+	// Item deletes iteself from world space after it has been added to inventory 
+	void HonorableSeppuku(){
+		Destroy(gameObject);
+		Destroy(this);
 	}
 
 }
