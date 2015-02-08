b62c5c3ca0a0e8078688340dda90b8bac91c7691
AOjemann <robintood@hotmail.com>
2015-02-08 15:31:07 -0500

    ani del, mouse fake
    basic animation delegator implemented, hooked vomit and eat into master,
    added some basic mouse touch fake controls

--- GetUpChuckUnityProject/Assets/Scripts/MasterCtrl.cs
+++ GetUpChuckUnityProject/Assets/Scripts/MasterCtrl.cs
@@ -5,16 +5,25 @@ public class MasterCtrl : MonoBehaviour {
 	//stored reference to red chuck
 	GameObject RedCK;
 	Player RedCKPlayer;
+	Inventory RedInv;
 
 	//stored reference to blue chuck
 	GameObject BlueCK;
 	Player BlueCKPlayer;
+	Inventory BlueInv;
 
 	// Tracks which player is currently active
 	public GameObject activePlayer_go;
 	Player activePlayer;
+	Inventory activeInventory;
 
+	//camera info
+	Camera mainCam;
 	public CameraPan cameraPan;
+
+	//tracks position for mouse drags
+	Vector2 mouseDragStart;
+	Vector2 defMDS = new Vector2 (-1.0f, -1.0f);
 
 	// Use this for initialization
 	void Start () {
@@ -22,20 +31,25 @@ public class MasterCtrl : MonoBehaviour {
 		RedCK = GameObject.FindGameObjectWithTag("redCK");
 		RedCKPlayer = RedCK.GetComponent<Player> ();
 		BlueCKPlayer = BlueCK.GetComponent<Player> ();
-		cameraPan = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraPan>();
+		RedInv = RedCK.GetComponent<Inventory> ();
+		BlueInv = BlueCK.GetComponent<Inventory> ();
+		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		cameraPan = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<CameraPan>();
 		setActivePlayer("red");
 		setActivePlayerGO("red");
+		setInventory ("red");
+		mouseDragStart = defMDS;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("CKleft")){
+		/*if (Input.GetButton("CKleft")){
 			walkLeft();
 		}
 		if (Input.GetButton("CKright")){
 			walkRight();
-		}
+		}*/
 		if (Input.GetButtonDown("CKjumpL")){
 			jumpLeft();
 		}
@@ -45,6 +59,54 @@ public class MasterCtrl : MonoBehaviour {
 		if (Input.GetButtonDown("playerSwaps")){
 			swapPlayer();
 		}
+
+		//TODO Add eat/vomit controls
+		//hook
+
+		if (Input.GetMouseButton (0)) {
+			if (mouseDragStart == defMDS){
+				if (0 < Input.mousePosition.x && Input.mousePosition.x < (mainCam.pixelWidth/3)){
+					walkLeft();
+				}
+				if ((2*mainCam.pixelWidth/3) < Input.mousePosition.x && Input.mousePosition.x < mainCam.pixelWidth){
+					walkRight();
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			if ((mainCam.pixelWidth/3) < Input.mousePosition.x && Input.mousePosition.x < (2*mainCam.pixelWidth/3)){
+				mouseDragStart = Input.mousePosition;
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			if (mouseDragStart != defMDS){
+				if (0 < Input.mousePosition.x && Input.mousePosition.x < (mainCam.pixelWidth/3)){
+					jumpLeft();
+				}
+				if ((2*mainCam.pixelWidth/3) < Input.mousePosition.x && Input.mousePosition.x < mainCam.pixelWidth){
+					jumpRight();
+				}
+			}
+			mouseDragStart = defMDS;
+		}
+		/*foreach (Touch touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began) {
+				if (0 < touch.position.x < (mainCam.pixelWidth/3)){
+					walkLeft();
+				}
+				if ((2*mainCam.pixelWidth/3) < touch.position.x < mainCam.pixelWidth){
+					walkRight();
+				}
+				// Construct a ray from the current touch coordinates
+				//var ray = Camera.main.ScreenPointToRay (touch.position);
+				//if (Physics.Raycast (ray)) {
+				//	// Create a particle if hit
+				//	Instantiate (particle, transform.position, transform.rotation);
+				//}
+			}
+		}*/
 	}
 
 	public void setActivePlayerGO(string color){
@@ -73,6 +135,19 @@ public class MasterCtrl : MonoBehaviour {
 		return this.activePlayer;
 	}
 
+	public void setInventory(string color){
+		if(color.Equals("red")){
+			activeInventory = RedInv;
+		}
+		if(color.Equals("blue")){
+			activeInventory = BlueInv;
+		}
+	}
+
+	public Inventory getActiveInventory(){
+		return this.activeInventory;
+	}
+
 	//INPUT TAGS
 	//CKleft
 	//CKright
@@ -86,26 +161,41 @@ public class MasterCtrl : MonoBehaviour {
 	//Walks left
 	void walkLeft(){
 		activePlayer.moveLeft();
+		//masterAnimationDel ("walkLeft");
 	}
 
 	//walk right
 	void walkRight(){
 		activePlayer.moveRight();
+		//masterAnimationDel ("walkRight");
 	}
 
 	//jump left
 	void jumpLeft(){
 		activePlayer.jump("left");
+		masterAnimationDel ("jumpLeft");
 	}
 
 	//jump right
 	void jumpRight(){
 		activePlayer.jump("right");
+		masterAnimationDel ("jumpRight");
 	}
 
 	//called when player dies
 	void playerDeath (){
 		activePlayer.playerRe();
+		masterAnimationDel ("death");
+	}
+
+	void playerEat(Item tmpI){
+		activeInventory.AddItem (tmpI);
+		masterAnimationDel ("eat");
+	}
+
+	void playerUpChuck(Item tmpI){
+		activeInventory.DropItem (tmpI);
+		masterAnimationDel ("upchuck");
 	}
 
 	//called to switch player from red to blue or blue to red
@@ -113,14 +203,59 @@ public class MasterCtrl : MonoBehaviour {
 		if (activePlayer == RedCKPlayer) {
 			setActivePlayer("blue");
 			setActivePlayerGO("blue");
+			setInventory("blue");
 			cameraPan.TriggerPan(BlueCK, RedCK);
 		}
 		else if (activePlayer == BlueCKPlayer) {
 			setActivePlayer("red");
 			setActivePlayerGO("red");
+			setInventory("red");
 			cameraPan.TriggerPan(RedCK, BlueCK);
 		}
 	}
 
+	//function to delegate animations to animators in specific objects
+	void masterAnimationDel (string aniAction){
+		//TODO hook up with real animation scrips
+		//TODO handle walking animation using x velosity calculation
+		//disable if jumping, use y caculation similar to doulbe jump
+		if (aniAction.Equals("jumpRight")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.jumpRight();
+		}
+		if (aniAction.Equals("jumpLeft")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.jumpLeft();
+		}
+		//if (aniAction.Equals("walkLeft")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.walkLeft();
+		//}
+		//if (aniAction.Equals("walkRight")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.walkRight();
+		//}
+		if (aniAction.Equals("death")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.death();
+		}
+		if (aniAction.Equals("STwalkLeft")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.STwalkLeft();
+		}
+		if (aniAction.Equals("STwalkRight")){
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.STwalkRight();
+		}
+		if (aniAction.Equals ("eat")) {
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.eat();
+		}
+		if (aniAction.Equals ("upchuck")) {
+			//myAnimatorPlayer tmpAnimator = activePlayer_go.GetComponent<myAnimatorPlayer>();
+			//tmpAnimator.upchuck();
+		}
+
+	}
 
 }
