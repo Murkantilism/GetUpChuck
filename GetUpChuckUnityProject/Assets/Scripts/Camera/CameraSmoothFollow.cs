6e09aa2ceb57f85783a69d193cd3c8bdedd09034
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2015-02-08 22:12:01 -0500

    Large commit, see description
    -Organized art assets
    -Implemented camera zoom in/out for inventory
    -Implemented camera smooth follow
    -Implemented camera bg parallax effect
    -Created 'devScene' to test above scripts. This scene should be our
    template scene for testing new features and building levels off of.

---
+++ GetUpChuckUnityProject/Assets/Scripts/Camera/CameraSmoothFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Camera))] // This script should be attached to the main camera object
+public class CameraSmoothFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance = 3.0f;
+	public float height = 3.0f;
+	public float damping = 5.0f;
+	public bool smoothRotation = true;
+	public float rotationDamping = 10.0f;
+	public bool lockRotation;
+
+	MasterCtrl master;
+
+	void Start(){
+		master = GameObject.FindGameObjectWithTag("Master").GetComponent<MasterCtrl>();
+		target = master.getActivePlayerGO().transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		target = master.getActivePlayerGO().transform;
+		Vector3 targetPosition = target.TransformPoint(0, height, -distance);
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * damping);
+
+		if(smoothRotation == true){
+			Quaternion targetPosQuat = Quaternion.LookRotation(target.position - transform.position, target.up);
+			targetPosition = new Vector3(targetPosQuat.x, targetPosQuat.y, targetPosQuat.z);
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetPosQuat, Time.deltaTime * rotationDamping);
+		}else{
+			transform.LookAt(target, target.up);
+		}
+
+		if(lockRotation == true){
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		}
+	}
+}
