1b515a0e8a217a0ee6ef83d00753705d049588a4
slynch0t <slynchot@Gmail.com>
2015-02-11 21:52:46 -0500

    Adding Main Menu

---
+++ GetUpChuckUnityProject/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	private Animator anim;
+	public bool load = false;
+	
+	void Start() {
+		anim = this.GetComponent<Animator>();
+		anim.enabled = false;
+	}
+
+	void Update() {
+		if (load) {
+			Application.LoadLevel("UI_testScene");
+		}
+	}
+
+	public void LoadLevel() {
+		anim.enabled = true;
+		anim.Play ("LoadPanel");		
+	}
+}
