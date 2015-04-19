83d985b1899a9a21b9daf92502fa2d949758fc56
AOjemann <robintood@hotmail.com>
2015-04-19 15:09:30 -0400

    added next level cake
    added cake item functionality that saves the current level when eaten
    and loads the next level

---
+++ GetUpChuckUnityProject/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	//name of next level
+	public string NextLevelName;
+	public int thisLevelNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	public void goToNextLevel(){
+		PlayerPrefs.SetInt ("level", thisLevelNumber);
+		PlayerPrefs.Save();
+		Application.LoadLevel (NextLevelName);
+	}
+
+}
