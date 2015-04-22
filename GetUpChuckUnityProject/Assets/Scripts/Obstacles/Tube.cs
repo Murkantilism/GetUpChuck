c1ae4837476eb29fa6ac37a6dcb4dbdc10f9d544
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2015-04-22 12:13:55 -0400

    Added react props to LabLevel2_2, fixed nullrefexc

--- GetUpChuckUnityProject/Assets/Scripts/Obstacles/Tube.cs
+++ GetUpChuckUnityProject/Assets/Scripts/Obstacles/Tube.cs
@@ -9,7 +9,7 @@ public class Tube : MonoBehaviour, Obstacle {
 	private Inventory inv;
 
 	void Start(){
-		hud = GameObject.Find("MenuEvents").GetComponent<HUD>();
+		hud = GameObject.Find("UI_Handler").GetComponent<HUD>();
 	}
 
 	public void OnCollisionEnter2D(Collision2D coll) {
