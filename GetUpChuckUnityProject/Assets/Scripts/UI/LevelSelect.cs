0868e79d9991d5211931f7e163039ad968f4ebb0
slynch0t <slynchot@Gmail.com>
2015-04-28 18:54:39 -0400

    unlcoking levels from playerprefs

--- GetUpChuckUnityProject/Assets/Scripts/UI/LevelSelect.cs
+++ GetUpChuckUnityProject/Assets/Scripts/UI/LevelSelect.cs
@@ -4,27 +4,21 @@ using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour {
 
-	public int levelReached;
 	private GameObject level;
 	private GameObject lockBox;
-	private Button butt;
 	private Animator anim;
+	private int levelReached;
 
-	// Use this for initialization
 	void Start () {
 		anim = this.gameObject.GetComponent<Animator> ();
 		anim.Play ("Show_Levels");
+		levelReached = PlayerPrefs.GetInt("level") + 1;
 		UnlockLevels ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	void UnlockLevels() {
-		for (int i=1; i<= levelReached; i++) {
-			level = GameObject.Find("Lvl" + i.ToString());
+		for (int i=2; i <= levelReached; i++) {
+			level = GameObject.Find("Lvl" + i);
 			level.GetComponent<Button>().interactable = true;
 			lockBox = level.transform.GetChild (0).gameObject;
 			GameObject.Destroy (lockBox);
